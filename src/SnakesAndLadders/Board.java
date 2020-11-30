f55df893151aad34f2c656c6970c3210b706b4c8
User <JP@itt.ie>
2020-11-30 18:35:32 +0000

    cghk

--- src/SnakesAndLadders/Board.java
+++ src/SnakesAndLadders/Board.java
@@ -7,20 +7,20 @@ public class Board extends JFrame {
 
     public Board() {
         Dice dice = new Dice();
-        boolean gameOver=false;
-        int count=0;
+        boolean gameOver = false;
+        int count = 0;
 
-        int p1x=0,p1y=450,p2x=0,p2y=450;
-        int diceRoll, turn=0;
+        int p1x = 0, p1y = 450, p2x = 0, p2y = 450;
+        int diceRoll, turn = 0;
 
-        int boardWidth=600, boardHeight=500, tileSize=100;
+        int boardWidth = 600, boardHeight = 500, tileSize = 100;
 
-        boolean isRight=true;
+        boolean isRight = true;
 
 
-        while (gameOver==false){
+        while (gameOver == false) {
 
-            MYCanvas m = new MYCanvas(p1x,p1y,p2x,p2y);
+            MYCanvas m = new MYCanvas(p1x, p1y, p2x, p2y);
             boolean player1Turn = true;
             JFrame f = new JFrame();
             f.add(m);
@@ -28,66 +28,70 @@ public class Board extends JFrame {
             f.setVisible(true);
 
 
-            //int roll = (int) Math.round( (Math.random() * 4 + 1));
-            int roll = 2;
+            int roll = (int) Math.round( (Math.random() * 4 + 1));
+            //int roll = 3;
             //count++;
 
-            if (p1y<=0) {
-                gameOver=true;
+            if (p1y <= 0) {
+                gameOver = true;
             }
             diceRoll = roll * tileSize;
 
-           if (isRight){
-               p1x+=diceRoll;
-               if (p1x>=boardWidth){
+            if (isRight) {
 
-                   p1y-=tileSize;
-                   p1x+=diceRoll;
-                   p1x=diceRoll%p1x;
-                   isRight=false;
-               }
-           }
-           else
-           {
+                for (int i = 0; i < roll; i++) {
+                    if (isRight) {
+                        p1x += 100;
+                        if (p1x == 600) {
 
-                   p1x-=diceRoll;
-                   if (p1x<0){
-
-                       p1y-=tileSize;
-                       p1x=0;
-                       p1x+=diceRoll-tileSize;
-
-                       isRight=true;
-                   }
+                            isRight = false;
+                            p1y -= tileSize;
+                        }
+                    }
+                    if (!isRight) {
+                        p1x -= 100;
+                    }
 
 
+                }
 
-           }
+            } else {
+                for (int i = 0; i < roll; i++) {
+                    if (!isRight) {
+                        p1x -= 100;
+                        if (p1x == -100) {
+
+                            isRight = true;
+                            p1y -= tileSize;
+                        }
+                    }
+                    if (isRight) {
+                        p1x += 100;
+                    }
 
 
-            m.setP1x(p1x);
-            m.setP1y(p1y);
-            player1Turn = false;
-            System.out.println("x"+diceRoll);
-            roll = (int) Math.round( (Math.random() * 4 + 1));
-            diceRoll = roll * 100;
-            p2x+=diceRoll;
-            m.setP2x(p2x);
+                }
+            }
 
-            System.out.println("y"+diceRoll);
+                m.setP1x(p1x);
+                m.setP1y(p1y);
+                player1Turn = false;
+                System.out.println("x" + diceRoll);
+                roll = (int) Math.round((Math.random() * 4 + 1));
+                diceRoll = roll * 100;
+                p2x += diceRoll;
+                m.setP2x(p2x);
+
+                System.out.println("y" + diceRoll);
 
 
-            String pause = JOptionPane.showInputDialog("test");
-            f.setVisible(false);
-            //f.repaint();
+                String pause = JOptionPane.showInputDialog("test");
+                f.setVisible(false);
+                //f.repaint();
+            }
+
+
         }
-
-
-
-
-
-    }
-
 
 
     }
---
+++ src/SnakesAndLadders/movePlayer.java
@@ -0,0 +1,4 @@
+package SnakesAndLadders;
+
+public class movePlayer {
+}
