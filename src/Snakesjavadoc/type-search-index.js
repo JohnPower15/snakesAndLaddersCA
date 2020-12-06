11c29478b0717fae2bb04a0721fc392c460a3e7f
User <JP@itt.ie>
2020-12-06 00:59:26 +0000

    fggd

---
+++ src/Snakesjavadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"SnakesAndLadders","l":"Board"},{"p":"SnakesAndLadders","l":"Dice"},{"p":"SnakesAndLadders","l":"FileSave"},{"p":"SnakesAndLadders","l":"MovePlayer"},{"p":"SnakesAndLadders","l":"MYCanvas"},{"p":"<Unnamed>","l":"testSandL"},{"p":"SnakesAndLadders","l":"testSnakesAndLadders"}]
